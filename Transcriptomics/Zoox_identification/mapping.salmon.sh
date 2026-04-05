1f44f6f7e5c447b99272f968c53ee6bc6359f492
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2026-04-05 17:23:28 +0300

    zoox mapping

---
+++ Transcriptomics/Zoox_identification/mapping.salmon.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#################################################################################################################
+#SBATCH --job-name=sbatchTemplate ## Name of your job
+#SBATCH --ntasks=8 ## number of cpu's to allocate for a job
+#SBATCH --ntasks-per-node=8 ## number of cpu's to allocate per each node
+#SBATCH --nodes=1 ## number of nodes to allocate for a job
+#SBATCH --mem=64G ## memory to allocate for your job in MB
+#SBATCH --time=1-00:00:00 ## time to allocate for your job in format: DD-HH:MM:SS
+#SBATCH --error=%J.errors ## stderr file name(The %J will print job ID number)
+#SBATCH --output=%J.output ## stdout file name(The %J will print job ID number)
+#SBATCH --mail-type=NONE ## Send your job status via e-mail: Valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
+########### Job information #############
+echo "================================"
+echo "Start at `date`"
+echo "Job id is $SLURM_JOBID"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NTASKS processors."
+echo "================================"
+#########################################
+
+######## Load required modules ##########
+#. /etc/profile.d/modules.sh # Required line for modules environment to work
+#module load openmpi/1.8.4 python/2.7 # Load modules that are required by your program
+#########################################
+source /lustre1/home/mass/eskalon/miniconda/bin/activate salmon
+### Below you can enter your program job command ###
+
+#salmon index -t symbionts_combined_prot.fa -i transc_index -k 31
+
+for f in raw/*mate1; do mv "$f" "$f.fastq"; done
+
+for R1 in raw/*mate1.fastq; do
+R2="${R1%.out.mate1.fastq}"
+
+echo "$R1"
+echo "$R2"
+
+salmon quant -i transc_index -l A -p 64 --fldMean 250 --fldSD 50 \
+ -r "$R1" -o "$R2" --validateMappings --gcBias --seqBias
+
+done
